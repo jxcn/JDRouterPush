ab4685ed91365acc1195b6eb02f78d6e2f62dc91
James Tieman <9104852+jxcn@users.noreply.github.com>
2022-07-12 09:40:19 +0800

    Update NoticeTemplate.py

--- NoticeTemplate.py
+++ NoticeTemplate.py
@@ -2,9 +2,8 @@
 def normalTemplate():
     content = """{content}---
 数据日期:{date}
-今日总收益:{total_today}
+昨日总收益:{total_today}
 总可用积分:{avail_today}
-绑定账户:{account}
 设备总数:{devicesCount}
 **设备信息如下:**
 {detail}"""
@@ -22,7 +21,7 @@ def markdownTemplate():
 ```
 {date}
 ```
-**今日总收益:**
+**昨日总收益:**
 ```
 {total_today}
 ```
